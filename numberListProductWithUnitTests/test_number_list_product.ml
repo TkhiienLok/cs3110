bd917745a803b2f9bbaed2adb3483322bf8c1492
Tkhiien Lok Chan <tkhiien@gmail.com>
2024-07-11 20:14:53 +0200

    passed printer argument to assertions, made a util for creating test cases

--- numberListProductWithUnitTests/test_number_list_product.ml
+++ numberListProductWithUnitTests/test_number_list_product.ml
@@ -2,12 +2,15 @@
 open OUnit2
 open Number_list_product
 
+let make_number_list_product_test name expected_output input =
+  name >:: (fun _ -> assert_equal expected_output (number_list_product input) ~printer:string_of_int)
+
 let tests = "test suite for number_list_product" >::: [
-  "empty" >:: (fun _ -> assert_equal 1 (number_list_product []));
-  "singleton" >:: (fun _ -> assert_equal 2 (number_list_product [2]));
-  "two_elements" >:: (fun _ -> assert_equal 2 (number_list_product [1; 2]));
-  "with_non_even_number_of_negative" >:: (fun _ -> assert_equal (-6) (number_list_product [2; 3; -1]));
-  "with_even_number_of_negative" >:: (fun _ -> assert_equal (10) (number_list_product [-5; -2;]));
+  make_number_list_product_test "empty" 1 [];
+  make_number_list_product_test "singleton" 2  [2];
+  make_number_list_product_test "two_elements" 6  [2; 3];
+  make_number_list_product_test "with_non_even_number_of_negative" (-8)  [2; 4; -1];
+  make_number_list_product_test "with_even_number_of_negative" 10  [-5; -2];
 ]
 
 let _ = run_test_tt_main tests
